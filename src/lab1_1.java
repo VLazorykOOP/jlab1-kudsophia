4212d083920103fa8730f6c57db9cfb4449f9dd3
Sofiia <kudrynska.sofiia@chnu.edu.ua>
2023-02-10 20:24:07 +0200

    added double to int

--- src/lab1_1.java
+++ src/lab1_1.java
@@ -20,10 +20,24 @@ public class lab1_1 {
         int b2 = num2_2.nextInt();
         int res2 = (a2/(b2*b2 - 2)) + (b2/(a2*a2 + 2));
         double Res2 = res2;
-        System.out.print("Res = " + Res2);
+        System.out.println("Res = " + Res2);
+
+        System.out.println("3.double to int:");
+        System.out.print("a = ");
+        Scanner num3_1 = new Scanner(System.in);
+        double a3 = num3_1.nextDouble();
+        System.out.print("b = ");
+        Scanner num3_2 = new Scanner(System.in);
+        double b3 = num3_2.nextDouble();
+        double res3 = (a3/(b3*b3 - 2)) + (b3/(a3*a3 + 2));
+        int Res3 = (int) res3;
+        System.out.println("Res = " + Res3);
+
         num1_1.close();
         num1_2.close();
         num2_1.close();
         num2_2.close();
+        num3_1.close();
+        num3_2.close();
     }
 }
