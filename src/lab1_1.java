4098c50a32899a95d45146bd326e12f5e7359a40
Sofiia <kudrynska.sofiia@chnu.edu.ua>
2023-02-10 19:45:32 +0200

    double to double

--- src/lab1_1.java
+++ src/lab1_1.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
 public class lab1_1 {
-
     public static void main(String[] args) {
-        System.out.println("Lab 1 Java");
+        System.out.print("a = ");
+        Scanner num1 = new Scanner(System.in);
+        double a = num1.nextDouble();
+        System.out.print("b = ");
+        Scanner num2 = new Scanner(System.in);
+        double b = num2.nextDouble();
+        double res = (a/(b*b - 2)) + (b/(a*a + 2));
+        System.out.print("Res = " + res);
+
     }
 }
