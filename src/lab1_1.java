59f50513bdf7dd5ac0c35bf84c21daabb089bbfc
Sofiia <kudrynska.sofiia@chnu.edu.ua>
2023-02-10 20:10:37 +0200

    added int to double

--- src/lab1_1.java
+++ src/lab1_1.java
@@ -1,14 +1,29 @@
 import java.util.Scanner;
 public class lab1_1 {
     public static void main(String[] args) {
+        System.out.println("1.double to double:");
         System.out.print("a = ");
-        Scanner num1 = new Scanner(System.in);
-        double a = num1.nextDouble();
+        Scanner num1_1 = new Scanner(System.in);
+        double a1 = num1_1.nextDouble();
         System.out.print("b = ");
-        Scanner num2 = new Scanner(System.in);
-        double b = num2.nextDouble();
-        double res = (a/(b*b - 2)) + (b/(a*a + 2));
-        System.out.print("Res = " + res);
+        Scanner num1_2 = new Scanner(System.in);
+        double b1 = num1_2.nextDouble();
+        double res1 = (a1/(b1*b1 - 2)) + (b1/(a1*a1 + 2));
+        System.out.println("Res = " + res1);
 
+        System.out.println("2.int to double:");
+        System.out.print("a = ");
+        Scanner num2_1 = new Scanner(System.in);
+        int a2 = num2_1.nextInt();
+        System.out.print("b = ");
+        Scanner num2_2 = new Scanner(System.in);
+        int b2 = num2_2.nextInt();
+        int res2 = (a2/(b2*b2 - 2)) + (b2/(a2*a2 + 2));
+        double Res2 = res2;
+        System.out.print("Res = " + Res2);
+        num1_1.close();
+        num1_2.close();
+        num2_1.close();
+        num2_2.close();
     }
 }
