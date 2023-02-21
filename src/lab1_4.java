a4bb297bf41f2e1f31e7b9340df77ff754dd1548
Sofiia <kudrynska.sofiia@chnu.edu.ua>
2023-02-22 00:21:58 +0200

    count occurencies, max count, word with max count

--- src/lab1_4.java
+++ src/lab1_4.java
@@ -1,22 +1,44 @@
 import java.util.Scanner;
 import java.io.*;
 public class lab1_4 {
+    public static int countOccurencies(String text, char symbol) {
+        int lastIndex = 0; //
+        int count = 0;
+        while (lastIndex != -1) {
+            lastIndex = text.indexOf(symbol, lastIndex);
+            if (lastIndex != -1) {
+                count++;
+                lastIndex++;
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введіть текст: ");
         String text = in.nextLine();
-        String[] words = text.split(" ,.:;-?!+");
+        String[] words = text.split(" ");
         System.out.println("Введіть символ або букву: ");
-        String symbol = in.nextLine();
-        System.out.println("З тексту: "+text+"\nсимвол '"+symbol+"' найбільшу кількість разів мають слова:\n");
+        //String symbol = in.nextLine();
+        Scanner sc = new Scanner(System.in);
+        char symbol = sc.next().charAt(0);
+        System.out.println("З тексту: " + text + "\nсимвол '" + symbol + "' найбільшу кількість разів мають слова:\n");
 
-        int count = 0;
-        for (int i=0; i<words.length; i++) {
-            String word = words[i];
-            if (word.charAt(i) == symbol) {
-                count++;
+        int maxCount = 0;
+        for (int i = 0; i < words.length; i++) {
+            int count = countOccurencies(words[i], symbol);
+            if (count > maxCount) {
+                maxCount = count;
             }
         }
-        System.out.println(count);
+        //System.out.println(maxCount);
+
+        for (int i = 0; i < words.length; i++) {
+            int count = countOccurencies(words[i], symbol);
+            if (count == maxCount) {
+                System.out.println(words[i]);
+            }
         }
+    }
 }
