e55bc2a0acc6f0499ff8bf5741bae8b199690095
Sofiia <kudrynska.sofiia@chnu.edu.ua>
2023-02-22 01:58:49 +0200

    .

--- src/lab1_2.java
+++ src/lab1_2.java
@@ -33,12 +33,68 @@ public class lab1_2 {
         }
         return j;
     }
+
+    public static int[] noRepeat2(int[] a){
+        int n = a.length;
+        int[] duplicates = new int[n];
+        int[] res = new int[n];
+        int k = 0;
+        for (int i=0; i<n; i++){
+            int found = 0;
+            for (int j=i+1; j<n; j++) {
+                if (a[i] == a[j]) {
+                    found++;
+                }
+            }
+            if (found > 0) {
+                duplicates[k++] = a[i];
+            }
+        }
+
+        k = 0;
+        for (int i=0; i<n; i++){
+            if (!inArray(duplicates, a[i])) {
+                res[k++] = a[i];
+            }
+        }
+
+        return res;
+    }
+
+    static boolean inArray(int[] a, int t) {
+        for (int i = 0; i < a.length; i++){
+            if (t == a[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static int maxElement(int[] a) { //вихідний масив
+        int max = a[0];
+        for (int i = 0; i < a.length; i++){
+            if (max < a[i]) {
+                max = a[i];
+            }
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         in = new Scanner(System.in);
         int[] myArray = Input();
         System.out.println("Вихідний масив: ");
         int length_1 = myArray.length;
         Print(myArray, length_1);
+
+        //----
+        int[] aa = noRepeat2(myArray);
+        for (int i=0; i<aa.length; i++) {
+            System.out.print(aa[i]+" ");
+        }
+        int max = maxElement(aa);
+        System.out.print("Max = " + max);
+        //----
 
         Arrays.sort(myArray); //сортує за зростанням
         int length_2 = myArray.length;
@@ -48,8 +104,8 @@ public class lab1_2 {
         for (int i=0; i<length_2; i++)
             System.out.print(myArray[i]+" ");
 
-        int max = myArray[length_2-1];
-        System.out.print("Максимальне значення = "+max);
+        //int max = myArray[length_2-1];
+        //System.out.print("Максимальне значення = "+max);
 
     }
 }
